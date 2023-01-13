519fa6c4062ebb2f04082504e89641c31c7b126c
Walker <walker.m.oliver@gmail.com>
2023-01-13 10:55:55 -0500

    Render Nav and CV

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import Nav from './components/Nav';
+import CV from './components/CV';
 import './styles/App.css';
 
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <Nav />
+        <CV />
+      </div>
+    );
   }
 }
 
